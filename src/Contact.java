48eb430d94c045d3416743ac9882827ee847714c
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-19 14:31:31 +0530

    added contact section

---
+++ src/Contact.java
@@ -0,0 +1,31 @@
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone;
+
+    public Contact(String firstName, String lastName, String address, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    // Getters & Setters
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " | " + address + " | " +phone;
+}
+}
